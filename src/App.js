f6953b75141fa35aedff009cff9aab66bb19b917
shaniboy992 <sadiqzeeshan354@gmail.com>
2023-01-24 20:30:03 +0500

    deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginPage from './Components/LoginPage';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <LoginPage />
+  </div>
+  );
+}
+
+export default App;
